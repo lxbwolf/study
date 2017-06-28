14fcad611085e602a98c9b1c185907670a201069
Brooke Lau <lxb234@gmail.com>
2017-06-28 09:41:08 +0800

    Update cp_set_to_vec.cpp

--- stl/cp_set_to_vec.cpp
+++ stl/cp_set_to_vec.cpp
@@ -1,1 +1,20 @@
 
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+
+int main()
+{
+	std::set<double> input;
+    input.insert(5);
+    input.insert(6);
+    
+    std::vector<double> output(2); // Must be with a size.
+    std::copy(input.begin(), input.end(), output.begin());
+    
+    // If size is not assigned in intialization, back_inserter is also available.
+    std::copy(input.begin(), input.end(), std::back_inserter(output));
+    
+	return 0;
+}
