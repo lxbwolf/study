4ea0a9d723425c5df5bdefa9d76065ae9025048a
Brooke Lau <lxb234@gmail.com>
2017-06-28 09:40:43 +0800

    Update cp_vec_to_vec.cpp

--- stl/cp_vec_to_vec.cpp
+++ stl/cp_vec_to_vec.cpp
@@ -1,1 +1,23 @@
 
+#include <iostream>
+#include <iterator>
+#include <set>
+#include <vector>
+
+int main()
+{
+	std::vector<std::string> orig;
+    orig.push_back("first");
+    orig.push_back("second);
+    
+    std::vector<std::string> v;
+    
+    v = orig;
+    v.insert(v.end(), v.begin(), v.end());
+    // Now v contains: {"first", "second", "first", "second"}
+    
+    v = orig;
+    std::copy(v.begin(), v.end(), std::back_inserter(v));
+
+	return 0;
+}
