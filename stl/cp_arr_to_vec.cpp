ba4c1da7dc66bdd343622c0867369289cd657b97
Brooke Lau <lxb234@gmail.com>
2017-06-28 09:41:29 +0800

    Update cp_arr_to_vec.cpp

--- stl/cp_arr_to_vec.cpp
+++ stl/cp_arr_to_vec.cpp
@@ -1,1 +1,20 @@
 
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+int main(int arc, char* argv[])
+{
+	double darr[10] = {1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.6, 1.7, 1.8, 1.9};
+	std::vector<double> vdouble(10); // must be with a size.
+    auto it = vdouble.begin();
+    std::copy(darr, darr + 10, it);
+    
+    while (it != vdouble.end())
+    {
+    	std::cout << *it << std::endl;
+        ++ it;
+    }
+
+	return 0;
+}
